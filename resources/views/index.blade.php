a8094c21cacc858bf00f5de1d6848869c57815d3
最初のコミットです。 <>
2022-05-07 20:23:46 +0900

    SCSSでカラー変数設定、googleフォント読み込み済

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,53 +4,18 @@
 @section('title', 'Hello world!')
 
 @section('content')
-  <div class="main brown">
-    brown
-  </div>
-  <div class="part green">
-    green
-  </div>
-  <div class="part pink">
-    pink
-  </div>
-  <div class="main">
-    white
-  </div>
-  <div class="main brown">
-    brown
-  </div>
-  <div class="part green">
-    green
-  </div>
-  <div class="part pink">
-    pink
-  </div>
-  <div class="main">
-    white
-  </div>
-  <div class="main brown">
-    brown
-  </div>
-  <div class="part green">
-    green
-  </div>
-  <div class="part pink">
-    pink
-  </div>
-  <div class="main">
-    white
-  </div>
-  <div class="main brown">
-    brown
-  </div>
-  <div class="part green">
-    green
-  </div>
-  <div class="part pink">
-    pink
-  </div>
-  <div class="main">
-    white
-  </div>
+<h1>コーティー<br>パーティー</h1>
+<br>aaaaaaaa
+
+<br><br>
+aaaaaaaaaaaa
+
+<br><br><br>
+
+
+aaaaaaaaaaaaa
+
+
+
 
 @endsection
