d2aa7e11c62afa755f5c373b0d6f9ab7a839c825
最初のコミットです。 <>
2022-05-08 19:45:11 +0900

    npm run watchコマンドを更新毎にブラウザリロードするよう編集＆ヘッダー・フッター・記事パーツのbladeコンポ作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,11 @@
-@extends('app')
+@extends('layouts.app')
 
 
 @section('title', 'Hello world!')
 
 @section('content')
-<header class="header">
-  <h1 class="header-logo"><a href="{{route('index')}}">コーティー<br>パーティー</a></h1>
-
-  <nav class="nav-menu">
-    <ul class="menu">
-      <li class="menu-item">
-        <a class="menu-link" href="">新規登録</a>
-      </li>
-      <li class="menu-item">
-        <a class="menu-link" href="">ログイン</a>
-      </li>
-      <li class="menu-item">
-        <a class="menu-link" href="">マイページ</a>
-      </li>
-      <li class="menu-item">
-        <a class="menu-link" href="">投稿</a>
-      </li>
-    </ul>
-  </nav>
-</header>
-
+  @include('parts.header')
+  @include('parts.article')
+  @include('parts.footer')
 
 @endsection
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,20 @@
+<header class="header">
+  <h1 class="header-logo"><a href="{{route('index')}}">コーティー<br>パーティー</a></h1>
+
+  <nav class="nav-menu">
+    <ul class="menu">
+      <li class="menu-item">
+        <a class="menu-link" href="">新規登録</a>
+      </li>
+      <li class="menu-item">
+        <a class="menu-link" href="">ログイン</a>
+      </li>
+      <li class="menu-item">
+        <a class="menu-link" href="">マイページ</a>
+      </li>
+      <li class="menu-item">
+        <a class="menu-link" href="">投稿</a>
+      </li>
+    </ul>
+  </nav>
+</header>
