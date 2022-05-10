956c77bdb5a69561f38f65393c4917bb22c7c9ee
最初のコミットです。 <>
2022-05-10 20:00:03 +0900

    タイトルと記事のスタイルを少々効かせました。

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,18 @@
 <div class="wrapper">
   @include('parts.header')
   <main class="main">
-    @include('parts.article')
-
+    <div class="container">
+      <section class="section">
+        <div class="title-wrapper">
+          <h2 class="title">パーティー一覧</h2>
+        </div>
+        <div class="article-wrapper">
+          @include('parts.article')
+          @include('parts.article')
+          @include('parts.article')          
+        </div>
+      </section>
+    </div>
   </main>
   @include('parts.footer')
 </div>
--- resources/views/parts/article.blade.php
+++ resources/views/parts/article.blade.php
@@ -1,1 +1,3 @@
-記事だよ
+<article class="article">
+  <h3 class="article-title">素敵なカフェ探そう</h3>
+</article>
