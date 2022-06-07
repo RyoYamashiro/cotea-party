796e21fc174591da1acda05ab4670b06b8f65a04
最初のコミットです。 <>
2022-06-07 11:12:14 +0900

    usersテーブルのgenderをnullableにするため、マイグレーション編集と削除済み

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('self_introduction')->nullable();
-            $table->tinyInteger('gender')->default(0);
+            $table->tinyInteger('gender')->nullable(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
